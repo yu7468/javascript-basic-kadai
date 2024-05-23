83d7d9fa9cab0d33e4cf67a9aafcc9f5a93e9e38
yu7468 <cherish7468@gmail.com>
2024-05-23 17:09:21 +0900

    holiday.jsを追加

---
+++ kadai_009/holiday.js
@@ -0,0 +1,30 @@
+const holidays = [
+  "正月",
+  "成人の日",
+  "建国記念日",
+  "天皇誕生日",
+  "春分の日",
+  "昭和の日",
+  "憲法記念日",
+  "みどりの日",
+  "こどもの日",
+  "海の日",
+  "山の日",
+  "敬老の日",
+  "秋分の日",
+  "スポーツの日",
+  "文化の日",
+  "勤労感謝の日",
+];
+
+console.log("for文処理結果：");
+for (i = 1; i <= 16; i++) {
+  console.log(holidays[i]);
+}
+
+console.log("while文処理結果：");
+let j = 1;
+while (j <= holidays.length) {
+  console.log(holidays[j]);
+  j++;
+}
